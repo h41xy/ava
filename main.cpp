4415bebef8fda3839c4f627c93f3769f691c1344
h41 <urs.oberdorf@autistici.org>
2016-10-31 16:44:45 +0100

    everything starts with main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// The main file for the ava app
+
+int main () {
+
+	int run(){
+	// Read File
+	// ID IP:Port
+	//
+	// argv ID, lookup ID and listen on the port
+	//
+	// choose three other IDs
+	//
+	// output msgs with timestamp
+	//
+	// send ID to neighbours
+	//
+	// send msg with timestamp
+	}
+}
