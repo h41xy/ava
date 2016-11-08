84c8792a73cdc68b14f6fbeb10a34155a43ea79d
h41 <urs.oberdorf@autistici.org>
2016-11-08 09:34:31 +0100

    cleanup and comment addressbook cpp and h

--- include/addressbook.h
+++ include/addressbook.h
@@ -1,10 +1,14 @@
-// The Addressbook which contains the IDs, IPs and Ports
+// The addressbook contains Entries about neighbors.
+// Those include id, ip and port.
+// All get read from a given filepath and parsed into entries.
+// They get stored as a linked list.
 
 #include <string>
 #include <fstream>
 #include <list>
 #include <tuple>
-#include <ctime>
+#include <ctime> // included for random seed
+
 #include "entry.h"
 
 class Addressbook{
@@ -13,8 +17,8 @@ class Addressbook{
 	Addressbook(std::string);
 	void fillfromfile(std::string);
 	void add(Entry);
-	int remove(int);
-	Entry getbyid(int);
+	int remove(int); // Removes an Entry identified by id, return -1 if not found
+	Entry getbyid(int); // Returns found Entry, if not found returns an Entry with (-1,"",-1). pretty ulgy
 	int entrycount();
-	std::tuple<Entry,Entry,Entry> return_three_random_entries(int);
+	std::tuple<Entry,Entry,Entry> return_three_random_entries(int); //Returns three random entries
 };
--- src/addressbook.cpp
+++ src/addressbook.cpp
@@ -1,28 +1,34 @@
 // The Addressbook which contains the IDs, IPs and Ports
+// Those include id, ip and port.
+// All get read from a given filepath and parsed into entries.
+// They get stored as a linked list.
 
 #include "addressbook.h"
 
+// Read from given filename
 Addressbook::Addressbook(std::string fname){
 	fillfromfile(fname);
 }
 
+// Parser for translating the contains of the file to Entries.
+// Opens the file and looks for entries.
+// The entries have to be <id,whitespace,ip,colon,port>
 void Addressbook::fillfromfile(std::string fname){
-	// open file
-	// while != eof
-	// create entry from line
-	// add to addressbook
-	// close
+
 	std::ifstream ifile;
 	ifile.open(fname);
 
 	std::string line, ip;
 	int id, port;
+
+	// For every line
+	// find the seperating chars <whitespace, colon>
+	// build and store substrings based on the seperators
 	while( getline(ifile,line)){
 		std::size_t pos_white = line.find(" ");
 		std::size_t pos_colo = line.find(":");
 
 		id = std::stoi(line.substr(0, pos_white));
-		ip = line.substr(pos_white+1,((line.size()-(line.size()-pos_colo)-pos_white)-1));
 		ip = line.substr(pos_white+1,(line.size()-(pos_colo+pos_white)));
 		port = std::stoi(line.substr(pos_colo+1,((line.size()-pos_colo)-1)));
 
@@ -31,28 +37,33 @@ void Addressbook::fillfromfile(std::string fname){
 	ifile.close();
 }
 
-// Fill the Addressbook from a file
 void Addressbook::add(Entry nentry){
 	book.push_back(nentry);
 }
 
+// Removes an Entry from the addressbook identified by his id
+// returns -1 if not found.
 int Addressbook::remove(int id){
+	// Iterates over the list until the last entry is reached
 	std::list<Entry>::iterator it = book.begin();
 	while((*it).getid() != id){
-		it++;
 		if(it == book.end())
 			return -1;
+		it++; // put it in while( (++(*it).getid()) != id) ?
 	}
 	book.erase(it);
 	return 0;
 }
 
+// Returns a Entry identified by id
+// can be combined with remove when good return is found
 Entry Addressbook::getbyid(int id){
+	// Iterates over the list until the last entry is reached
 	std::list<Entry>::iterator it = book.begin();
 	while((*it).getid() != id){
 		it++;
 		if(it == book.end())
-			return Entry(-1,"",-1);
+			return Entry(-1,"",-1); // Return NULL like java not possible, altenative not found yet.
 	}
 	return (*it);
 }
@@ -61,6 +72,7 @@ int Addressbook::entrycount(){
 	return book.size();
 }
 
+// Returns three random entries fomr the current addressbook as a tuple
 std::tuple<Entry,Entry,Entry> Addressbook::return_three_random_entries(int id){
 	// random seed...kinda ; )
 	std::srand(std::time(0));
