d843cefe3c84765f0d63b45e1b9b9918e0ba024d
Urs Oberdorf <urs.oberdorf@autistici.org>
2016-11-23 12:32:50 +0100

    Removed static

--- include/constants.h
+++ include/constants.h
@@ -3,15 +3,16 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-static const int EXIT_NODE = 0;
-static const int EXIT_ALL = 1;
-static const int RECV_MSG = 2;
-static const int SOCIALISE = 3;
-static const int RUMOR = 4;
+const int EXIT_NODE = 0;
+const int EXIT_ALL = 1;
+const int RECV_MSG = 2;
+const int SOCIALISE = 3;
+const int RUMOR = 4;
 
-static const int MSG_BUFFER_SIZE = 256;
+const int MSG_BUFFER_SIZE = 256;
+const int WATCHER_PORT = 25000;
 
-static const std::string ADDRESSFILE = "doc/addresses.txt";
-static const std::string GRAPHFILE = "doc/graph.txt";
+const std::string ADDRESSFILE = "doc/addresses.txt";
+const std::string GRAPHFILE = "doc/graph.txt";
 
 #endif
