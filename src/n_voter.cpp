11d3a85637dd94d302f812a29f450ed21d253444
h41 <urs.oberdorf@autistici.org>
2017-01-26 10:45:00 +0100

    updated the README and output of terminated nodes

--- src/n_voter.cpp
+++ src/n_voter.cpp
@@ -191,14 +191,11 @@ int N_voter::vtime_check_terminate(std::vector<int>& cur_vtime, std::vector<int>
 	if (cur_vtime[myid - 1] >= term_vtime[myid - 1]){
 		if (!vtime_terminated){
 			// TODO no time to explain, get to da chopper
-			std::map<int, int>::iterator max_v_it = std::max_element(candidate_c_levels.begin(), candidate_c_levels.end());
-			int max = max_v_it->second;
-			int max_id = 0;
-			bool doublemax = false;
 			std::ostringstream os;
-			os << "1:" << candidate_c_levels[1] << " " << "2:" << candidate_c_levels[2];
-			find_id_of_max_value(candidate_c_levels, max_id, doublemax);
-			Message term_me(myself,NODE_TERMINATED,max_id,max,os.str());
+			// TODO hardcoded : /
+			os << "[C_ID:LVL 1:" << candidate_c_levels[1] << " " << "2:" << candidate_c_levels[2] << " ]";
+			os << "[TermTime: " << vtime[myid -1] << " ]";
+			Message term_me(myself,NODE_TERMINATED,-1,-1,os.str());
 			logger_signal_out(myself, term_me, true); 
 		}
 		vtime_terminated = true;
