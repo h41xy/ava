d7873e05784688c118229b81beee4cfe00cb052d
h41 <urs.oberdorf@autistici.org>
2017-02-06 22:29:28 +0100

    added run script

---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+SHAREDRES="doc/shared.res"
+MAIN="bin/node_exec"
+
+ADDRESSFILE="doc/addresses_all.txt"
+ADDRESSFILE_THIS_RUN="doc/addresses.txt"
+
+# Clean the shared file and init it
+reset_runlog(){
+	echo -n "" > $SHAREDRES
+	echo "0" > $SHAREDRES
+}
+
+reset_runlog
+
+if [ -z ${1+x} ]
+then
+	echo -n "Number of nodes: "
+	read NODES
+else
+	NODES=$1
+fi
+
+if [ ! $(($NODES % 2)) -eq 0 ]
+then
+	echo "Number of nodes must be even."
+	exit
+fi
+
+# Reduce the addressbook of 5000 Entries to just the number of nodes
+LINES=`cat $ADDRESSFILE | wc -l`
+cat $ADDRESSFILE | sed $((NODES+1)),${LINES}d > $ADDRESSFILE_THISRUN
+
+for (( i=0; i<=$NODES; i++))
+do
+	echo "Starting voter node with ID $i..."
+	./bin/node_exec $i &
+done
